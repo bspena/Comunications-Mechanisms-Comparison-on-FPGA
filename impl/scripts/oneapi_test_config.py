0605e1546fef03f38e329f6b9d7a5411530b6212
Vincenzo Maisto <maisto_v@libero.it>
2024-06-06 18:47:59 +0200

    Last updates before actual test

--- impl/scripts/oneapi_test_config.py
+++ impl/scripts/oneapi_test_config.py
@@ -4,19 +4,19 @@ import os
 # Test configuration and result files #
 #######################################
 # Path to test_list and test_resut
-test_list_path = os.getcwd() + '/test_list_r.csv'
+test_list_path = os.getcwd() + '/test_list.csv'
 # Test list file columns
 test_list_columns = ('sample_name','array_size')
 # Test result file columns 
 test_result_columns = ('array_size','latency','throughput')
 # Number of repetitions for each test
-test_list_num_repetitions = 3
+test_list_num_repetitions = 5
 
 ###################
 # Plot parameters #
 ###################
 # x-axis valus
-x_values = [1,16,256,1024,1048576,16777216]
+x_values = [1024,4096,8192,16384,65536,262144,1048576,2097152,8388608,]
 # Axis Labels
 x_label = 'Array Size (B)'
 y_label_latency = 'Latency (ms)'
@@ -28,7 +28,7 @@ legend_label_memch = 'USM Device Allocation'
 latency_plot_title = 'Comparison between Latencies'
 throughputs_plot_title = 'Comparison between Throughputs'
 # Number of indipendent factors
-number_if = 6
+number_if = 10
 
 ###################
 # oneAPI samples #
